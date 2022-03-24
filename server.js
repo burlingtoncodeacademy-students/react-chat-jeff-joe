024671964388339dd8d1bdfd3c24a777ce135ae8
[jeff priest] <[jspriest@yahoo.com]>
2022-03-24 13:13:26 -0400

    initial backend

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 8000;
-const app = express();
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(express.urlencoded({ extended:true }))
 
 const Message = require('./Message.js')
 

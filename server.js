d10a45fcd678637a44fa2db8816076eab3af147e
[jeff priest] <[jspriest@yahoo.com]>
2022-03-25 17:33:20 -0400

    Backend v.2.1

--- server.js
+++ server.js
@@ -27,11 +27,63 @@ db.mongoose
     process.exit();
   });
 
-
-// Basic route
-app.get('/', (req, res) => {
-    res.json("App is working!");
+// Get all messages
+app.get('/allmessages', (req, res) => {
+    Message.find({}, function(err, messages) {
+      let messageMap = {}
+      messages.forEach(function(message) {
+        messageMap[message._id] = message
+      })
+      res.send(messageMap)
+    })
 })
+
+// Post message
+app.post("/create", async (req, res) => {
+  if (room === 'room1') {
+    const newMessage = new Message ({
+        when: req.body.when,
+        name: req.body.name,
+        room1: true,
+        room2: false,
+        room3: false,
+        room4: false,
+        body: req.body.body
+    })
+    } else if (room === 'room2') {
+    const newMessage = new Message ({
+      when: req.body.when,
+      name: req.body.name,
+      room1: false,
+      room2: true,
+      room3: false,
+      room4: false,
+      body: req.body.body
+    })
+    } else if (room === 'room3') {
+    const newMessage = new Message ({
+      when: req.body.when,
+      name: req.body.name,
+      room1: false,
+      room2: false,
+      room3: true,
+      room4: false,
+      body: req.body.body    
+    })
+    } else if (room === 'room4') {
+    const newMessage = new Message ({
+      when: req.body.when,
+      name: req.body.name,
+      room1: false,
+      room2: false,
+      room3: false,
+      room4: true,
+      body: req.body.body       
+    })
+  }
+  await newMessage.save()
+  res.redirect("/")
+});
 
 // Set port and listen for requests
 const port = process.env.PORT || 5000;

d179a4766d5a6c52279c2d0a1f7e91757b759c73
Joe Walsh <joeevanwalsh@gmail.com>
2022-03-25 11:14:56 -0400

    react framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import { useState } from 'react/cjs/react.production.min';
 import './App.css';
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom';
-import {Room1, Room2, Room3} from './components'
-
+import Room1 from './components/room1.js';
+import Room2 from './components/room2.js';
+import Room3 from './components/room3.js';
+// const mongoose = require('mongoose')
+// mongoose.connect("localhost://27017/messages")
 function App() {
   return (
+    <main>
+          <style src="./App.css"></style>
     <div className="App">
       <>
       <div className="header">
         <h1>Chat With Mongoose !</h1>
       </div>
+      <div className="sidebarWrap">
       <div className="display">
         <h3>Current Room</h3>
         <div className="content">
@@ -25,25 +30,30 @@ function App() {
         </Route>
         </Routes>
         </div>
-      <input type='text' name='userName' id='userName' />
-      <input type='text' name='message' id='message' />
+      <form action='/create' method='POST'>
+      <input type='text' name='userName' placeholder='Display Name' />
+      <input type='text' name='message' placeholder='Send a Message' />
       <button type='submit'>SEND</button>
       <button type='submit'>REFRESH</button>
+      </form>
       </div>
       <div className="nav">
         <h3>Available Rooms</h3>
         <ul>
-        <h3>Rooms</h3>
+        <h4>Rooms</h4>
+      <div className='links'>
       <button><Link to='/'>Lobby</Link></button>
       <button><Link to='/room1'>Room 1</Link></button>
       <button><Link to='/room2'>Room 2</Link></button>
       <button><Link to='/room3'>Room 3</Link></button>
+      </div>
 
         </ul>
-
+      </div>
       </div>
       </>
     </div>
+    </main>
   );
 }
 
---
+++ client/src/components/room2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+export default function Room2() {
+
+    return(
+        <>
+            placeholder text
+
+
+
+        </>
+    )
+}
